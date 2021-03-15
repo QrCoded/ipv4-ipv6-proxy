871475f346e938311e8421d2896b2f3818312684
Thang Phi <37077554+thangpmedia@users.noreply.github.com>
2021-03-15 23:45:29 +0700

    Update install_centos8.sh
    Set 1000 proxies

--- scripts/install_centos8.sh
+++ scripts/install_centos8.sh
@@ -126,9 +126,9 @@ IP6=$(ip addr show dev ${NETWORK_INTERFACE_NAME} | sed -e's/^.*inet6 \([^ ]*\)\/
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}. Network interface name = ${NETWORK_INTERFACE_NAME}"
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
-
+#echo "How many proxy do you want to create? Example 500"
+#read COUNT
+COUNT=1000
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
